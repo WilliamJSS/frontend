6ac439468e4266f411dd596f0ec603860ae92e6c
WilliamJSS <williamsena088@gmail.com>
2022-09-13 20:34:52 -0300

    feat: primeira página com react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './pages/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header title="My User App" />
     <App />
   </React.StrictMode>
 );
