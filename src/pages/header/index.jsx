a9b005ad37efdfa832e831de21827a12857c2dbc
WilliamJSS <williamsena088@gmail.com>
2022-09-14 15:13:59 -0300

    refactor: extensão 'jsx' e pasta de rotas

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import "./style.css";
 
-export default function Header(props){
-    return(
+export default function Header(props) {
+    return (
         <header id="main-header">
             <h1>{props.title}</h1>
         </header>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import User from "./pages/user";
+import User from '../pages/user'
 
 export default function AppRoutes() {
     return (
