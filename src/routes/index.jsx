5b1ab3efc9d853192cfb9b818b68aa254eb16f73
WilliamJSS <williamsena088@gmail.com>
2022-09-21 13:05:20 -0300

    feat: adicionada página de cadastro

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import User from '../pages/user'
+import Profile from "../pages/profile";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<User />} />
+                <Route path="/create" element={<Profile />} />
             </Routes>
         </BrowserRouter>
     );
